30a13bac33c2e5960b2a4f06b8dad979c94d7c68
Wilie12 <albertw536@gmail.com>
2026-06-18 20:33:00 +0200

    chore: initial project setup and infrastructure configuration

---
+++ src/main/java/com/nn/ticketapp_api/TicketappApiApplication.java
@@ -0,0 +1,13 @@
+package com.nn.ticketapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nn/ticketapp_api/TicketappApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nn.ticketapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
