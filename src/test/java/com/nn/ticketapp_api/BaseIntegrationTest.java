e69a3bfe41d71441f83060cade735af713b1b2fd
Wilie12 <albertw536@gmail.com>
2026-07-24 15:10:46 +0200

    fix(core): resolve context load failures and enforce JPA entity hashing

--- src/test/java/com/nn/ticketapp_api/BaseIntegrationTest.java
+++ src/test/java/com/nn/ticketapp_api/BaseIntegrationTest.java
@@ -1,16 +1,13 @@
 package com.nn.ticketapp_api;
 
+import io.minio.MinioClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
-@Testcontainers
 public class BaseIntegrationTest {
 
     @ServiceConnection
@@ -22,10 +19,7 @@ public class BaseIntegrationTest {
 
     @MockitoBean
     protected JwtDecoder jwtDecoder;
+    @MockitoBean
+    protected MinioClient minioClient;
 
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("DB_USER", postgres::getUsername);
-        registry.add("DB_PASSWORD", postgres::getPassword);
-    }
 }
